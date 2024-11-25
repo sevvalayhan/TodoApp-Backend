f5e485f1d747acc2898af656c2918aae402313db
Sevval Ayhan <sevvalayhan2002@gmail.com>
2024-11-25 10:12:46 +0300

    gitignore file modified

--- ToDoAppBackend/todo_app_backend/todo_app_backend/settings.py
+++ ToDoAppBackend/todo_app_backend/todo_app_backend/settings.py
@@ -96,7 +96,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'todo_app_db',     
         'USER': 'root',         
-        'PASSWORD':'**',     
+        'PASSWORD': '**',     
         'HOST': 'localhost',               
         'PORT': '3306',                  
         'OPTIONS': { 'charset': 'utf8mb4', 'init_command': "SET sql_mode='STRICT_TRANS_TABLES', innodb_strict_mode=1", },
